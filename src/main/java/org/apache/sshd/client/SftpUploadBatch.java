69ee3c6af5f76f585a4685744fb6e6e7d22debf6
lau-ka-ho <asdf913@gmail.com>
2026-09-24 21:20:35 +0800

    Initial

---
+++ src/main/java/org/apache/sshd/client/SftpUploadBatch.java
@@ -0,0 +1,356 @@
+package org.apache.sshd.client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Proxy;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.function.FailableBiConsumer;
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableConsumer;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.sshd.client.future.AuthFuture;
+import org.apache.sshd.client.future.ConnectFuture;
+import org.apache.sshd.client.keyverifier.AcceptAllServerKeyVerifier;
+import org.apache.sshd.client.keyverifier.ServerKeyVerifier;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.client.session.ClientSessionCreator;
+import org.apache.sshd.common.future.VerifiableFuture;
+import org.apache.sshd.common.session.Session;
+import org.apache.sshd.common.session.SessionHolder;
+import org.apache.sshd.sftp.client.SftpClient;
+import org.apache.sshd.sftp.client.SftpClientFactory;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class SftpUploadBatch {
+
+	public static void main(final String[] args) throws Exception {
+		//
+		final Map<String, String> map = toMap(args);
+		//
+		try (final SshClient sshClient = SshClient.setUpDefaultClient()) {
+			//
+			setServerKeyVerifier(sshClient, AcceptAllServerKeyVerifier.INSTANCE);
+			//
+			start(sshClient);
+			//
+			try (final ClientSession clientSession = testAndApply(
+					(a, b) -> Boolean.logicalAnd(a != null, StringUtils.isNotEmpty(b)), get(map, "user"),
+					get(map, "host"),
+					(a, b) -> getSession(verify(connect(sshClient, a, b, NumberUtils.toInt(get(map, "port"), 22)))),
+					null)) {
+				//
+				testAndAccept(Objects::nonNull, get(map, "password"), x -> addPasswordIdentity(clientSession, x));
+				//
+				final File file = testAndApply(Objects::nonNull, get(map, "file"), File::new, null);
+				//
+				final StringBuilder remoteFolder = testAndApply(Objects::nonNull, get(map, "remoteFolder"),
+						StringBuilder::new, null);
+				//
+				append(append(remoteFolder, '/'), getName(file));
+				//
+				try (final SftpClient sftpClient = isSuccess(verify(auth(clientSession)))
+						? createSftpClient(SftpClientFactory.instance(), clientSession)
+						: null;
+						final InputStream is = testAndApply(Objects::nonNull, file, FileInputStream::new, null);
+						final OutputStream os = write(sftpClient, Objects.toString(remoteFolder))) {
+					//
+					testAndAccept((a, b) -> Boolean.logicalAnd(a != null, b != null), is, os, IOUtils::copy);
+					//
+				} // try
+					//
+			} // try
+				//
+		} // try
+			//
+	}
+
+	private static StringBuilder append(final StringBuilder instance, final char c) {
+		//
+		if (instance == null) {
+			//
+			return instance;
+			//
+		} // if
+			//
+		final Field field = testAndApply(x -> size(x) == 1,
+				collect(filter(
+						stream(testAndApply(Objects::nonNull, getClass(instance), FieldUtils::getAllFieldsList, null)),
+						f -> Objects.equals(getName(f), "value")), Collectors.toList()),
+				x -> get(x, 0), null);
+		//
+		return field == null || Narcissus.getField(instance, field) != null ? instance.append(c) : instance;
+		//
+	}
+
+	private static StringBuilder append(final StringBuilder instance, final Object obj) {
+		//
+		if (instance == null) {
+			//
+			return instance;
+			//
+		} // if
+			//
+		final Field field = testAndApply(x -> size(x) == 1,
+				collect(filter(
+						stream(testAndApply(Objects::nonNull, getClass(instance), FieldUtils::getAllFieldsList, null)),
+						f -> Objects.equals(getName(f), "value")), Collectors.toList()),
+				x -> get(x, 0), null);
+		//
+		return field == null || Narcissus.getField(instance, field) != null ? instance.append(obj) : instance;
+		//
+	}
+
+	private static String getName(final File instance) {
+		return instance != null && instance.getPath() != null ? instance.getName() : null;
+	}
+
+	private static <T, U, E extends Exception> void testAndAccept(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiConsumer<T, U, E> consumer) throws E {
+		if (predicate != null && predicate.test(t, u) && consumer != null) {
+			consumer.accept(t, u);
+		}
+	}
+
+	private static OutputStream write(final SftpClient instance, final String path) throws IOException {
+		return instance != null ? instance.write(path) : null;
+	}
+
+	private static void setServerKeyVerifier(final ClientAuthenticationManager instance,
+			final ServerKeyVerifier serverKeyVerifier) {
+		if (instance != null) {
+			instance.setServerKeyVerifier(serverKeyVerifier);
+		}
+	}
+
+	private static <T, U, R, E extends Exception> R testAndApply(final BiPredicate<T, U> predicate, final T t,
+			final U u, final FailableBiFunction<T, U, R, E> functionTrue,
+			final FailableBiFunction<T, U, R, E> functionFalse) throws E {
+		return predicate != null && predicate.test(t, u) ? apply(functionTrue, t, u) : apply(functionFalse, t, u);
+	}
+
+	private static <T, U, R, E extends Exception> R apply(final FailableBiFunction<T, U, R, E> instance, final T t,
+			final U u) throws E {
+		return instance != null ? instance.apply(t, u) : null;
+	}
+
+	private static SftpClient createSftpClient(final SftpClientFactory instnace, final ClientSession session)
+			throws IOException {
+		return instnace != null ? instnace.createSftpClient(session) : null;
+	}
+
+	private static ConnectFuture connect(final ClientSessionCreator instance, final String username, final String host,
+			final int port) throws IOException {
+		return instance != null ? instance.connect(username, host, port) : null;
+	}
+
+	private static AuthFuture auth(final ClientSession instance) throws IOException {
+		return instance != null ? instance.auth() : null;
+	}
+
+	private static <T, E extends Exception> void testAndAccept(final Predicate<T> predicate, final T value,
+			final FailableConsumer<T, E> consumer) throws E {
+		if (test(predicate, value)) {
+			accept(consumer, value);
+		}
+	}
+
+	private static <T, E extends Exception> void accept(final FailableConsumer<T, E> instance, final T value) throws E {
+		if (instance != null) {
+			instance.accept(value);
+		}
+	}
+
+	private static void addPasswordIdentity(final ClientAuthenticationManager instance, final String password) {
+		if (instance != null) {
+			instance.addPasswordIdentity(password);
+		}
+	}
+
+	private static void start(final SshClient instance) {
+		//
+		if (instance == null) {
+			//
+			return;
+			//
+		} // if
+			//
+		final Field field = testAndApply(x -> size(x) == 1,
+				collect(filter(
+						stream(testAndApply(Objects::nonNull, getClass(instance), FieldUtils::getAllFieldsList, null)),
+						f -> Objects.equals(getName(f), "state")), Collectors.toList()),
+				x -> get(x, 0), null);
+		//
+		if (field == null || (Narcissus.getField(instance, field)) != null) {
+			//
+			instance.start();
+			//
+		} // if
+			//
+	}
+
+	private static boolean isSuccess(final AuthFuture instance) {
+		return instance != null && instance.isSuccess();
+	}
+
+	private static <S extends Session> S getSession(final SessionHolder<S> instance) {
+		return instance != null ? instance.getSession() : null;
+	}
+
+	private static <T> T verify(final VerifiableFuture<T> instnace) throws IOException {
+		return instnace != null ? instnace.verify() : null;
+	}
+
+	private static <V> V get(final Map<?, V> instance, final Object key) {
+		return instance != null ? instance.get(key) : null;
+	}
+
+	private static Map<String, String> toMap(final String... ss) {
+		//
+		Map<String, String> map = null;
+		//
+		Entry<String, String> entry = null;
+		//
+		for (int i = 0; i < length(ss); i++) {
+			//
+			if ((entry = toEntry(ArrayUtils.get(ss, i))) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), getKey(entry), getValue(entry));
+			//
+		} // for
+			//
+		return map;
+		//
+	}
+
+	private static Entry<String, String> toEntry(final String string) {
+		//
+		final Field field = testAndApply(x -> size(x) == 1,
+				collect(filter(
+						stream(testAndApply(Objects::nonNull, getClass(string), FieldUtils::getAllFieldsList, null)),
+						f -> Objects.equals(getName(f), "value")), Collectors.toList()),
+				x -> get(x, 0), null);
+		//
+		if (string != null && field != null && Narcissus.getField(string, field) == null) {
+			//
+			return null;
+			//
+		} // if
+			//
+		if (Objects.equals(string, "=")) {
+			//
+			return Pair.of("", "");
+			//
+		} else if (string != null && string.length() == 2 && string.charAt(0) == '=') {
+			//
+			return Pair.of("", string.substring(1, string.length()));
+			//
+		} else if (string != null && string.length() == 2 && string.charAt(string.length() - 1) == '=') {
+			//
+			return Pair.of(string.substring(0, string.length() - 1), "");
+			//
+		} else if (string != null && string.indexOf('=') >= 0 && string.indexOf('=') == string.lastIndexOf('=')) {
+			//
+			return Pair.of(StringUtils.substringBefore(string, '='), StringUtils.substringAfter(string, '='));
+			//
+		} else if (string != null && string.length() > 2 && string.indexOf('=') != string.lastIndexOf('=')) {
+			//
+			return Pair.of(StringUtils.substring(string, 0, string.indexOf('=')),
+					StringUtils.substring(string, string.indexOf('=') + 1));
+			//
+		} // if
+			//
+		return null;
+		//
+	}
+
+	private static int length(final Object[] instance) {
+		return instance != null ? instance.length : 0;
+	}
+
+	private static <K> K getKey(final Entry<K, ?> instance) {
+		return instance != null ? instance.getKey() : null;
+	}
+
+	private static <V> V getValue(final Entry<?, V> instance) {
+		return instance != null ? instance.getValue() : null;
+	}
+
+	private static <K, V> void put(final Map<K, V> instance, final K key, final V value) {
+		if (instance != null) {
+			instance.put(key, value);
+		}
+	}
+
+	private static int size(final Collection<?> instance) {
+		return instance != null ? instance.size() : 0;
+	}
+
+	private static <E> E get(final List<E> instance, final int index) {
+		return instance != null ? instance.get(index) : null;
+	}
+
+	private static String getName(final Member instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static <T, R, A> R collect(final Stream<T> instance, final Collector<? super T, A, R> collector) {
+		return instance != null && (collector != null || Proxy.isProxyClass(getClass(instance)))
+				? instance.collect(collector)
+				: null;
+	}
+
+	private static <T> Stream<T> filter(final Stream<T> instance, final Predicate<? super T> predicate) {
+		return instance != null ? instance.filter(predicate) : instance;
+	}
+
+	private static <T> Stream<T> stream(final Collection<T> instance) {
+		return instance != null ? instance.stream() : null;
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	private static <T, R, E extends Throwable> R testAndApply(final Predicate<T> predicate, final T value,
+			final FailableFunction<T, R, E> functionTrue, final FailableFunction<T, R, E> functionFalse) throws E {
+		return test(predicate, value) ? apply(functionTrue, value) : apply(functionFalse, value);
+	}
+
+	private static <T> boolean test(final Predicate<T> instance, final T value) {
+		return instance != null && instance.test(value);
+	}
+
+	private static <T, R, E extends Throwable> R apply(final FailableFunction<T, R, E> instance, final T value)
+			throws E {
+		return instance != null ? instance.apply(value) : null;
+	}
+
+}
---
+++ src/test/java/org/apache/sshd/client/SftpUploadBatchTest.java
@@ -0,0 +1,525 @@
+package org.apache.sshd.client;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.nio.file.FileSystems;
+import java.nio.file.Paths;
+import java.security.KeyStoreException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.function.FailableBiConsumer;
+import org.apache.commons.lang3.function.FailableBiFunction;
+import org.apache.commons.lang3.function.FailableFunction;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.sshd.client.future.AuthFuture;
+import org.apache.sshd.client.session.ClientSession;
+import org.apache.sshd.client.session.ClientSessionCreator;
+import org.apache.sshd.common.file.virtualfs.VirtualFileSystemFactory;
+import org.apache.sshd.common.future.VerifiableFuture;
+import org.apache.sshd.common.session.SessionHolder;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.shell.ProcessShellCommandFactory;
+import org.apache.sshd.server.shell.ProcessShellFactory;
+import org.apache.sshd.sftp.client.SftpClient;
+import org.apache.sshd.sftp.client.SftpClientFactory;
+import org.apache.sshd.sftp.server.SftpSubsystemFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.google.common.reflect.Reflection;
+
+import io.github.toolfactory.narcissus.Narcissus;
+
+public class SftpUploadBatchTest {
+
+	private static Method METHOD_GET_NAME, METHOD_TEST_AND_ACCEPT, METHOD_IS_SUCCESS, METHOD_COLLECT = null;
+
+	@BeforeClass
+	static void beforeClass() throws Throwable {
+		//
+		final Class<?> clz = SftpUploadBatch.class;
+		//
+		(METHOD_GET_NAME = clz.getDeclaredMethod("getName", Member.class)).setAccessible(true);
+		//
+		(METHOD_TEST_AND_ACCEPT = clz.getDeclaredMethod("testAndAccept", BiPredicate.class, Object.class, Object.class,
+				FailableBiConsumer.class)).setAccessible(true);
+		//
+		(METHOD_IS_SUCCESS = clz.getDeclaredMethod("isSuccess", AuthFuture.class)).setAccessible(true);
+		//
+		(METHOD_COLLECT = clz.getDeclaredMethod("collect", Stream.class, Collector.class)).setAccessible(true);
+		//
+	}
+
+	private static class IH implements InvocationHandler {
+
+		private Boolean test, isSuccess;
+
+		private Integer size;
+
+		@Override
+		public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+			//
+			final String name = getName(method);
+			//
+			if (Objects.equals(method != null ? method.getReturnType() : null, Void.TYPE)) {
+				//
+				return null;
+				//
+			} // if
+				//
+			if (proxy instanceof Collection) {
+				//
+				if (Objects.equals(name, "size")) {
+					//
+					return size;
+					//
+				} else if (Objects.equals(name, "stream")) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} // if
+				//
+			if (Boolean.logicalAnd(proxy instanceof Map, contains(Arrays.asList("get", "put"), name))) {
+				//
+				return null;
+				//
+			} else if (Boolean.logicalOr(proxy instanceof Predicate, proxy instanceof BiPredicate)
+					&& Objects.equals(name, "test")) {
+				//
+				return test;
+				//
+			} else if (Boolean.logicalOr(proxy instanceof FailableFunction, proxy instanceof FailableBiFunction)
+					&& Objects.equals(name, "apply")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof Stream) {
+				//
+				if (contains(Arrays.asList("collect", "filter"), name)) {
+					//
+					return null;
+					//
+				} // if
+					//
+			} else if (proxy instanceof List && Objects.equals(name, "get")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof Member && Objects.equals(name, "getName")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof Entry && contains(Arrays.asList("getValue", "getKey"), name)) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof VerifiableFuture && Objects.equals(name, "verify")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof SessionHolder && Objects.equals(name, "getSession")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof AuthFuture && Objects.equals(name, "isSuccess")) {
+				//
+				return isSuccess;
+				//
+			} else if (proxy instanceof ClientSession && Objects.equals(name, "auth")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof ClientSessionCreator && Objects.equals(name, "connect")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof SftpClientFactory && Objects.equals(name, "createSftpClient")) {
+				//
+				return null;
+				//
+			} else if (proxy instanceof SftpClient && Objects.equals(name, "write")) {
+				//
+				return null;
+				//
+			} // if
+				//
+			throw new Throwable(name);
+			//
+		}
+
+	}
+
+	private SshServer sshServer = null;
+
+	private IH ih = null;
+
+	@BeforeMethod
+	void beforeMethod() throws IllegalAccessException, InvocationTargetException, KeyStoreException, IOException {
+		//
+		if ((sshServer = SshServer.setUpDefaultServer()) != null) {
+			//
+			sshServer.setHost("127.0.0.1");
+			//
+			sshServer.setPort(2222);
+			//
+			sshServer.setKeyPairProvider(new SimpleGeneratorHostKeyProvider());
+			//
+			sshServer.setPasswordAuthenticator((username, password, session) -> Objects.equals(username, "user")
+					&& Objects.equals(password, "password"));
+			//
+			sshServer.setCommandFactory(new ProcessShellCommandFactory());
+			//
+			final String name = getName(getClass(FileSystems.getDefault()));
+			//
+			if (contains(Arrays.asList("sun.nio.fs.MacOSXFileSystem", "sun.nio.fs.LinuxFileSystem"), name)) {
+				//
+				sshServer.setShellFactory(new ProcessShellFactory("/bin/sh", "-i"));
+				//
+			} else {
+				//
+				throw new IllegalStateException(name);
+				//
+			} // if
+				//
+			sshServer.setSubsystemFactories(Collections.singletonList(new SftpSubsystemFactory()));
+			//
+			sshServer.setFileSystemFactory(new VirtualFileSystemFactory(Paths.get("/")));
+			//
+			sshServer.start();
+			//
+		} // if
+			//
+		ih = new IH();
+		//
+	}
+
+	@AfterMethod
+	void afterMethod() throws IOException {
+		//
+		if (sshServer != null) {
+			//
+			sshServer.close();
+			//
+		} // if
+			//
+	}
+
+	private static String getName(final Class<?> instance) {
+		return instance != null ? instance.getName() : null;
+	}
+
+	private static boolean contains(final Collection<?> instance, final Object item) {
+		return instance != null && instance.contains(item);
+	}
+
+	private static String getName(final Member instance) throws Throwable {
+		try {
+			final Object obj = invoke(METHOD_GET_NAME, null, instance);
+			if (obj == null) {
+				return null;
+			} else if (obj instanceof String) {
+				return (String) obj;
+			}
+			throw new Throwable(Objects.toString(getClass(instance)));
+		} catch (final InvocationTargetException e) {
+			throw e.getTargetException();
+		}
+	}
+
+	@Test
+
+	void testNull() throws Throwable {
+		//
+		final Method[] ms = SftpUploadBatch.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Object result = null;
+		//
+		String toString = null;
+		//
+		Collection<Object> collection = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if (Objects.equals(parameterType = ArrayUtils.get(parameterTypes, j), Integer.TYPE)) {
+					//
+					add(collection, Integer.valueOf(0));
+					//
+				} else if (Objects.equals(parameterType, Character.TYPE)) {
+					//
+					add(collection, Character.valueOf(' '));
+					//
+				} else {
+					//
+					add(collection, null);
+					//
+				} // if
+					//
+			} // for
+				//
+			result = Narcissus.invokeStaticMethod(m, toArray(collection));
+			//
+			toString = Objects.toString(m);
+			//
+			if (contains(Arrays.asList(Integer.TYPE, Boolean.TYPE), m.getReturnType())) {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+
+	}
+
+	private static <E> void add(final Collection<E> instance, final E item) {
+		if (instance != null) {
+			instance.add(item);
+		}
+	}
+
+	private static void clear(final Collection<?> instance) {
+		if (instance != null) {
+			instance.clear();
+		}
+	}
+
+	private static Object[] toArray(final Collection<?> instance) {
+		return instance != null ? instance.toArray() : null;
+	}
+
+	@Test
+	void testNotNull() throws Throwable {
+		//
+		final Method[] ms = SftpUploadBatch.class.getDeclaredMethods();
+		//
+		Method m = null;
+		//
+		Class<?>[] parameterTypes = null;
+		//
+		Class<?> parameterType = null;
+		//
+		Object result = null;
+		//
+		String toString, name = null;
+		//
+		Collection<Object> collection = null;
+		//
+		for (int i = 0; ms != null && i < ms.length; i++) {
+			//
+			if ((m = ArrayUtils.get(ms, i)) == null || m.isSynthetic()
+					|| (parameterTypes = m.getParameterTypes()) == null) {
+				//
+				continue;
+				//
+			} // if
+				//
+			clear(collection = ObjectUtils.getIfNull(collection, ArrayList::new));
+			//
+			for (int j = 0; j < parameterTypes.length; j++) {
+				//
+				if ((parameterType = ArrayUtils.get(parameterTypes, j)) != null && parameterType.isInterface()) {
+					//
+					if ((ih = ObjectUtils.getIfNull(ih, IH::new)) != null) {
+						//
+						final List<Field> fs = FieldUtils.getAllFieldsList(getClass(ih));
+						//
+						Field f = null;
+						//
+						for (int k = 0; fs != null && k < fs.size(); k++) {
+							//
+							if ((f = fs.get(k)) == null) {
+								//
+								continue;
+								//
+							} // if
+								//
+							final Class<?> type = f.getType();
+							//
+							if (Objects.equals(type, Boolean.class)) {
+								//
+								Narcissus.setField(ih, f, Boolean.TRUE);
+								//
+							} else if (Objects.equals(type, Integer.class)) {
+								//
+								Narcissus.setField(ih, f, Integer.valueOf(0));
+								//
+							} // if
+								//
+						} // for
+							//
+					} // if
+						//
+					add(collection, Reflection.newProxy(parameterType, ih = ObjectUtils.getIfNull(ih, IH::new)));
+					//
+				} else if (parameterType != null && parameterType.isArray()) {
+					//
+					add(collection, Array.newInstance(parameterType.getComponentType(), 0));
+					//
+				} else if (Objects.equals(parameterType, Integer.TYPE)) {
+					//
+					add(collection, Integer.valueOf(0));
+					//
+				} else if (Objects.equals(parameterType, Character.TYPE)) {
+					//
+					add(collection, Character.valueOf(' '));
+					//
+				} else {
+					//
+					add(collection, Narcissus.allocateInstance(parameterType));
+					//
+				} // if
+					//
+			} // for
+				//
+			result = Narcissus.invokeStaticMethod(m, toArray(collection));
+			//
+			toString = Objects.toString(m);
+			//
+			if (contains(Arrays.asList(Boolean.TYPE, Integer.TYPE), m.getReturnType())
+					|| Boolean.logicalAnd(Objects.equals(name = getName(m), "getClass"),
+							Arrays.equals(parameterTypes, new Class<?>[] { Object.class }))
+					|| Boolean.logicalAnd(Objects.equals(name, "append"), Boolean.logicalOr(
+							Arrays.equals(parameterTypes, new Class<?>[] { StringBuilder.class, Object.class }),
+							Arrays.equals(parameterTypes, new Class<?>[] { StringBuilder.class, Character.TYPE })))) {
+				//
+				Assert.assertNotNull(result, toString);
+				//
+			} else {
+				//
+				Assert.assertNull(result, toString);
+				//
+			} // if
+				//
+		} // for
+			//
+	}
+
+	private static Class<?> getClass(final Object instance) {
+		return instance != null ? instance.getClass() : null;
+	}
+
+	@Test
+	void testMain() throws Exception {
+		//
+		SftpUploadBatch.main(new String[] { null, "=", "= ", " =", "== " });
+		//
+		final String host = sshServer != null ? sshServer.getHost() : null;
+		//
+		final String user = "user";
+		//
+		final RandomStringUtils randomStringUtils = RandomStringUtils.secure();
+		//
+		final File file = File.createTempFile(randomStringUtils != null ? randomStringUtils.nextAlphanumeric(3) : null,
+				null, new File("."));
+		//
+		if (file != null && file.exists()) {
+			//
+			FileUtils.copyFile(new File("pom.xml"), file);
+			//
+			file.deleteOnExit();
+			//
+		} // if
+			//
+		SftpUploadBatch.main(new String[] { "host=" + host, "port=2222", "user=" + user, "password=password",
+				"file=" + getAbsolutePath(file),
+				"remoteFolder=" + getAbsolutePath(file != null ? file.getParentFile() : null) });
+		//
+		FileUtils.deleteQuietly(file);
+		//
+	}
+
+	private static String getAbsolutePath(final File instance) {
+		return instance != null ? instance.getAbsolutePath() : null;
+	}
+
+	private static Object invoke(final Method method, final Object instance, final Object... args)
+			throws IllegalAccessException, InvocationTargetException {
+		return method != null && method.getDeclaringClass() != null ? method.invoke(instance, args) : null;
+	}
+
+	@Test
+	void testTestAndAccpt() throws IllegalAccessException, InvocationTargetException {
+		//
+		if ((ih = ObjectUtils.getIfNull(ih, IH::new)) != null) {
+			//
+			ih.test = Boolean.TRUE;
+			//
+		} // if
+			//
+		Assert.assertNull(
+				invoke(METHOD_TEST_AND_ACCEPT, null, Reflection.newProxy(BiPredicate.class, ih), null, null, null));
+		//
+	}
+
+	@Test
+	void testIsSuccess() throws IllegalAccessException, InvocationTargetException {
+		//
+		if ((ih = ObjectUtils.getIfNull(ih, IH::new)) != null) {
+			//
+			ih.isSuccess = Boolean.FALSE;
+			//
+		} // if
+			//
+		Assert.assertEquals(invoke(METHOD_IS_SUCCESS, null, Reflection.newProxy(AuthFuture.class, ih)),
+				ih != null ? ih.isSuccess : null);
+		//
+	}
+
+	@Test
+	void testCollect() throws IllegalAccessException, InvocationTargetException {
+		//
+		Assert.assertNull(invoke(METHOD_COLLECT, null, Stream.empty(), null));
+		//
+		Assert.assertNull(invoke(METHOD_COLLECT, null, Reflection.newProxy(Stream.class, ih), null));
+		//
+	}
+
+}
